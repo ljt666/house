8948846de9b77ceef4f7dd1a18c8755fad0a309c
ljt <2966936669@qq.com>
2023-01-01 15:29:52 +0800

    运用nacos配置中心

--- src/main/java/com/ljt/house/HouseApplication.java
+++ src/main/java/com/ljt/house/HouseApplication.java
@@ -1,10 +1,12 @@
 package com.ljt.house;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId="dev-house",groupId = "dev-house-ljt",autoRefreshed=true)
 @MapperScan("com.ljt.house.persistence")
 public class HouseApplication {
 
